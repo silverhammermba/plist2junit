a9d676af0afb9fdd8331458b5141c0d8e71e9456
Maxwell Anselm <manselm@lutron.com>
2019-05-15 13:33:28 -0400

    multithread coverage conversion

--- xccov2sonar.rb
+++ xccov2sonar.rb
@@ -1,30 +1,46 @@
 #!/usr/bin/env ruby
 require 'json'
+require 'stringio'
 
 # a better version of SonarQube's xccov-to-sonarqube-generic.sh
 
 if ARGV.length != 1
-  warn "usage: #$0 action.xccovarchive"
+  warn "usage: #$0 whatever.xccovarchive"
   exit 1
 end
 
 coverage_pattern = /\A\s*(\d+):\s*(\d+)/
 
-puts '<coverage version="1">'
-IO.popen(%w{xcrun xccov view --file-list} << ARGV[0]) do |paths|
-  paths.each_line do |path|
-    # TODO: this is absolute. want path relative to cwd
-    path.strip!
-    puts "<file path=#{path.encode xml: :attr}>"
-    IO.popen(%w{xcrun xccov view --file} << path << ARGV[0]) do |coverage|
-      open = false
+paths = nil
+IO.popen(%w{xcrun xccov view --file-list} << ARGV[0]) do |list|
+  paths = list.read.split("\n")
+end
 
-      coverage.each_line do |metrics|
-        if metrics =~ coverage_pattern
-          puts "<lineToCover lineNumber=\"#$1\" covered=\"#{$2 != ?0}\"/>"
+threads = paths.map do |path|
+  Thread.new do
+    io = StringIO.new
+    # TODO: this is absolute. want path relative to cwd
+    io.puts "<file path=#{path.encode xml: :attr}>"
+    begin
+      IO.popen(%w{xcrun xccov view --file} << path << ARGV[0]) do |coverage|
+        coverage.each_line do |metrics|
+          if metrics =~ coverage_pattern
+            io.puts "<lineToCover lineNumber=\"#$1\" covered=\"#{$2 != ?0}\"/>"
+          end
         end
       end
+    # too many files open, just wait
+    rescue Errno::EMFILE, Errno::ENFILE
+      sleep 1
+      retry
     end
-    puts '</file>'
+    io.puts '</file>'
+    io.string
   end
 end
+
+puts '<coverage version="1">'
+threads.each do |thread|
+  puts thread.value
+end
+puts '</coverage>'
